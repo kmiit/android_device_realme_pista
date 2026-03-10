33611cf7cbd218ffa8f3afd970216c67dfa0908a
kmiit <kmiit@foxmail.com>
2026-03-10 15:59:47 +0000

    pista: Adapt proprietary files list to 11.F.68
    Co-authored-by: Giovanni Ricca <giovanniricca@proton.me>
    Change-Id: I21bab3ecdc3404cb0bc0aac5951d5b3b4244fa22

--- extract-files.py
+++ extract-files.py
@@ -29,13 +29,12 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'(fdSupport += )TRUE;', r'\1FALSE;'),
     'odm/etc/init/init.camera_process.rc': blob_fixup()
         .regex_replace('    delete_recursion', '    #delete_recursion'),
-    'odm/firmware/fastchg/23821/charging_hyper_mode_config.txt': blob_fixup()
-        .regex_replace(r"(PROJECT:=)23893", r"\g<1>23821"),
     'odm/lib64/libAlgoProcess.so': blob_fixup()
         .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
     (
         'odm/lib64/libAncHumanSegFigureFusion.so',
         'odm/lib64/libEIS.so',
+        'odm/lib64/libFaceBeautyJni.so',
         'odm/lib64/libHIS.so',
         'odm/lib64/libOPAlgoCamAiBeautyFaceRetouchCn.so',
         'odm/lib64/libOPAlgoCamAiUnifySkin.so',
--- lineage_pista.mk
+++ lineage_pista.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := RMX5010
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi-user 16 BP2A.250605.015 1775846053839 release-keys" \
-    BuildFingerprint=realme/RMX5010/RE6018L1:16/BP2A.250605.015/V.35625fc-11fe4c9-122cb1a:user/release-keys \
+    BuildDesc="qssi-user 16 BP2A.250605.015 1786355200789 release-keys" \
+    BuildFingerprint=realme/RMX5010/RE6018L1:16/BP2A.250605.015/V.2a20db9-4ff7c3-520e61:user/release-keys \
     DeviceName=RE6018L1 \
     DeviceProduct=RMX5010 \
     SystemDevice=RE6018L1 \
